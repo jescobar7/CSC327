21065e0f3b2f355c79d27e51f199fa2f5fba2f5c
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-04-02 23:16:50 -0500

    Parity app can now read and display input.in file contents

--- Week 1/4167 - Parity/Parity.java
+++ Week 1/4167 - Parity/Parity.java
@@ -1,5 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+
 class Parity {
-	public static void main(String[] args) {
-		System.out.println("4167 - Parity");
+	public static void main(String[] args) throws IOException {
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(new BufferedReader(new FileReader("input.in")));
+			while (sc.hasNext()) {
+				System.out.println(sc.next());
+			}
+		} 
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
 	}
 }
