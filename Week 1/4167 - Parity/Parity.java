f247839a7318e949b0fdfe1e7e30de78816d631c
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-04-03 19:27:08 -0500

    reverted back to previous solution

--- Week 1/4167 - Parity/Parity.java
+++ Week 1/4167 - Parity/Parity.java
@@ -1,29 +1,53 @@
+import java.io.*;
 import java.util.Scanner;
 
-public class Parity2 {
-	
-	public static void main (String[] args) throws Exception {
-		Scanner s = new Scanner(System.in);
+public class Parity {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = null;
 		
-		while (true) {
-			String line = s.nextLine();
-			if(line.equals("#"))
-				break;
-			int parity = 0;
-			for (char c: line.toCharArray()) {
-				switch (c) {
-					case '1':
-						parity = 1-parity;
-					case '0':
-						System.out.print(c);
-						break;
-					case 'e':
-						System.out.println(parity);
-						break;
-					case 'o':
-						System.out.println(1-parity);
-						break;
+		try {
+			sc = new Scanner(new BufferedReader(new FileReader("input2.in")));
+			while (sc.hasNext()) {
+				String line = sc.next();
+				int parity = 0;
+				String output = "";
+				
+				for (int i = 0; i < line.length() && i < 31; i++) {
+					switch (line.charAt(i)) {
+						case '0':
+							output = output + "0";
+							break;
+						case '1':
+							parity += 1;
+							output = output + "1";
+							break;
+						case 'e':
+							if (parity % 2 == 0) {
+								output = output + "0";
+							} else {
+								output = output + "1";
+							}
+							parity = 0;
+							break;
+						case 'o':
+							if (parity % 2 == 1) {
+								output = output + "0";
+							} else {
+								output = output + "1";
+							}
+							parity = 0;
+							break;
+						case '#':
+							break;
+					}
 				}
+				System.out.println(output);
+				output = "";
+			}
+		} 
+		finally {
+			if (sc != null) {
+				sc.close();
 			}
 		}
 	}
