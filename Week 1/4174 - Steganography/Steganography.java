9ce4da54ed3dff9b54f4f7fb5b9f0be8179b3c93
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-04-06 15:41:31 -0500

    correctly counts number of spaces in example input text

--- Week 1/4174 - Steganography/Steganography.java
+++ Week 1/4174 - Steganography/Steganography.java
@@ -1,5 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Steganography {
-	public static void main(String[] args) {
-		System.out.println("4174 - Steganography");
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		boolean flag = true;
+		while (flag) {
+			String line = sc.nextLine();
+			char[] cArray = line.toCharArray();
+			
+			int spaces = 0;
+			for (int i = 0; i < cArray.length; i++) {
+				if (cArray[i] == '#') {
+					flag = false;
+				}
+				switch (cArray[i]) {
+					case ' ':
+						//System.out.print("Space");
+						spaces += 1;
+						break;
+					case '*':
+						//System.out.print("---");
+						break;
+				}
+				System.out.print(cArray[i]);
+			}
+			System.out.println("[Space Count = " + spaces + "]");
+			spaces = 0;
+			//System.out.println();
+		}
 	}
 }
