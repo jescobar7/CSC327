ecd4f70c024c9e913f2aedfe49c885e1b8d9c149
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-04-07 13:55:02 -0500

    correctly encodes spaces into binary

--- Week 1/4174 - Steganography/Steganography.java
+++ Week 1/4174 - Steganography/Steganography.java
@@ -8,27 +8,46 @@ public class Steganography {
 		boolean flag = true;
 		while (flag) {
 			String line = sc.nextLine();
-			char[] cArray = line.toCharArray();
 			
 			int spaces = 0;
-			for (int i = 0; i < cArray.length; i++) {
-				if (cArray[i] == '#') {
+			boolean checked = false;
+			String bString = "";
+			
+			for (char c: line.toCharArray()) {
+				if (c == '#') {
 					flag = false;
 				}
-				switch (cArray[i]) {
+				switch (c) {
 					case ' ':
-						//System.out.print("Space");
+						checked = false;
 						spaces += 1;
+						//System.out.print("[SPACE]");
 						break;
 					case '*':
-						//System.out.print("---");
+						//System.out.print("[*]");
+						break;
+					default:
+						if (checked == false) {
+							if (spaces > 0) {
+								//bString += "Z";
+								if (spaces % 2 == 0) {
+									bString += "1";
+									//System.out.print("["+c+"]");
+								}
+								else if (spaces % 2 == 1) {
+									bString += "0";
+									//System.out.print("["+c+"]");
+								}
+							}
+						}
+						spaces = 0;
+						checked = true;
 						break;
 				}
-				System.out.print(cArray[i]);
 			}
-			System.out.println("[Space Count = " + spaces + "]");
-			spaces = 0;
-			//System.out.println();
+			//int value = Integer.parseInt(bString, 2);
+			//System.out.println(Integer.parseInt(bString, 2));
+			System.out.println(bString);
 		}
 	}
 }
