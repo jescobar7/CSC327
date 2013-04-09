a92d99d553b9f3ab7245847260487f840eb06735
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-04-09 11:50:34 -0500

    get coordinates of shoes and firefly and recognize when it escapes

--- Week 1/4168 - Lampyridae Teleportae/Firefly.java
+++ Week 1/4168 - Lampyridae Teleportae/Firefly.java
@@ -6,15 +6,35 @@ public class Firefly {
 		Scanner sc = new Scanner(System.in);	
 		boolean run = true;
 		int firefly = 1;
+		int fX = 0;
+		int fY = 0;
+		int sT = 0;
+		int sX = 0;
+		int sY = 0;
 				
 		while (run) {
 			String line = sc.nextLine();
 			String[] split = line.split("\\s+");
 			
-			if ((split.length == 3) && (Integer.parseInt(split[0]) == 0)) {
+			int splitLen = split.length;
+			if ((splitLen == 3) && (Integer.parseInt(split[0]) == 0)) {
 				run = false;
 			} else {
+				if (splitLen == 3) {
+					sT = Integer.parseInt(split[0]);
+					sX = Integer.parseInt(split[1]);
+					sY = Integer.parseInt(split[2]);
+					System.out.println(sT + " " + sX + " " + sY);
+				} else if (splitLen == 2) {
+					fX = Integer.parseInt(split[0]);
+					fY = Integer.parseInt(split[1]);
+					System.out.println(fX + " " + fY);
+				}
 				
+				// if ((fX == -1) && (fY == -1)) {
+					// System.out.println("Firefly " + firefly + " not caught");
+					// firefly++;
+				// }
 			}
 		}
 	}
