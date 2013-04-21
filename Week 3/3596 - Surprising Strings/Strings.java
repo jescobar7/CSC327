81bc6ee0c9dde98aafcbd0e3a3a4f9120e2634dc
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-04-21 14:30:35 -0500

    initialized 3596 - Surprising Strings App

---
+++ Week 3/3596 - Surprising Strings/Strings.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Strings {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+	}
+}
