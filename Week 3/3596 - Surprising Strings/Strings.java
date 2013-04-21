5a8d044296c704bfb5ec283121ecf65a0540f1f8
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-04-21 18:50:48 -0500

    optimized code, but still wrong answer

--- Week 3/3596 - Surprising Strings/Strings.java
+++ Week 3/3596 - Surprising Strings/Strings.java
@@ -7,78 +7,79 @@ public class Strings {
 		Scanner sc = new Scanner(System.in);
 		boolean run = true;
 		char[] chars;
-		char EMPTY = ' ';
 		char STAR = '*';
 		
 		while (run) {
 			chars = sc.nextLine().toCharArray();
+			chars = charString(chars).toCharArray();
 			
-			if (chars[0] == STAR) {
-				run = false;
-			}
-			else if (chars[1] == EMPTY) {
-				System.out.println(chars[0]+" is surprising.");
-			}
-			else {
-				HashMap<String, String> oneMap = new HashMap<String, String>();
-				HashMap<String, String> twoMap = new HashMap<String, String>();
-				HashMap<String, String> threeMap = new HashMap<String, String>();
-				boolean oneUnique = true;
-				boolean twoUnique = true;
-				boolean threeUnique = true;
-				
-				for (int i = 0; i < 79; i++) {
-					if (oneUnique) {
-						if (i+1 < chars.length) {
-							String newKey = "";
-							newKey = newKey + chars[i];
-							newKey = newKey + chars[i+1];
-							if (!oneMap.containsKey(newKey)) {
-								oneMap.put(newKey, newKey);
-							} else {
-								oneUnique = false;
-							}
-						}
-					}
-					if (twoUnique) {
-						if (i+2 < chars.length) {
-							String newKey = "";
-							newKey = newKey + chars[i];
-							newKey = newKey + chars[i+2];
-							if (!twoMap.containsKey(newKey)) {
-								twoMap.put(newKey, newKey);
-							} else {
-								twoUnique = false;
-							}
-						}
-					}
-					if (threeUnique) {
-						if (i+3 < chars.length) {
-							String newKey = "";
-							newKey = newKey + chars[i];
-							newKey = newKey + chars[i+3];
-							if (!threeMap.containsKey(newKey)) {
-								threeMap.put(newKey, newKey);
-							} else {
-								threeUnique = false;
-							}
-						}
-					}
+			if (chars.length > 0 && chars.length <= 79) {
+				if (chars[0] == STAR) {
+					run = false;
 				}
-				
-				if (oneUnique == true && twoUnique == true && threeUnique == true) {
-					for (char c: chars) {
-						System.out.print(c);
-					}
-					System.out.print("is surprising.\n");
+				else if (chars.length == 1) {
+					System.out.println(charString(chars)+" is surprising.");
 				}
 				else {
-					for (char c: chars) {
-						System.out.print(c);
+					HashMap<String, String> zeroMap = new HashMap<String, String>();
+					HashMap<String, String> oneMap = new HashMap<String, String>();
+					HashMap<String, String> twoMap = new HashMap<String, String>();
+					boolean zeroUnique = true;
+					boolean oneUnique = true;
+					boolean twoUnique = true;
+					int len = chars.length;
+					
+					for (int i = 0; i < len; i++) {
+						if (zeroUnique) {
+							if (i+1 < len) {
+								String newKey = Character.toString(chars[i]) + chars[i+1];
+								if (!(zeroMap.containsKey(newKey))) {
+									zeroMap.put(newKey, newKey);
+								} else {
+									zeroUnique = false;
+								}
+							}
+						}
+						if (oneUnique) {
+							if (i+2 < len) {
+								String newKey = Character.toString(chars[i]) + chars[i+2];
+								if (!(oneMap.containsKey(newKey))) {
+									oneMap.put(newKey, newKey);
+								} else {
+									oneUnique = false;
+								}
+							}
+						}
+						if (twoUnique) {
+							if (i+3 < len) {
+								String newKey = Character.toString(chars[i]) + chars[i+3];
+								if (!(twoMap.containsKey(newKey))) {
+									twoMap.put(newKey, newKey);
+								} else {
+									twoUnique = false;
+								}
+							}
+						}
 					}
-					System.out.print("is NOT surprising.\n");
+					
+					if (zeroUnique && oneUnique && twoUnique) {
+						System.out.println(charString(chars)+" is surprising.");
+					}
+					else {
+						System.out.println(charString(chars)+" is NOT surprising.");
+					}
 				}
 			}
 		}
+	}
+	
+	public static String charString(char[] cArray) {
+		String cString = "";
+		for (char c: cArray) {
+			if (c != ' ') {
+				cString = cString + c;
+			}
+		}
+		return cString;
 	}
 }
