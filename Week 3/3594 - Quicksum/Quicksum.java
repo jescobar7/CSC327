e9b02acef2e6c7c02fd2d0e8d640df6c270ca1a7
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-04-21 23:36:49 -0500

    [3594 - Quicksum] Working Solution Submission

--- Week 3/3594 - Quicksum/Quicksum.java
+++ Week 3/3594 - Quicksum/Quicksum.java
@@ -4,9 +4,25 @@ import java.util.Scanner;
 public class Quicksum {
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);	
+		boolean run = true;
 		
-		while (sc.hasNextLine()) {
-			System.out.println(sc.nextLine());
+		while (run) {
+			String line = sc.nextLine();
+			char[] cArray = line.toCharArray();
+			
+			if (cArray[0] == '#') {
+				run = false;
+				break;
+			}
+			int i = 1;
+			int result = 0;
+			for (char c: cArray) {
+				if (c != ' ') {
+					result = result + (i *((int)c-64));
+				}
+				i++;
+			}
+			System.out.println(result);
 		}
 	}
 }
