bad5e9a1dac167f1e2db7cc0c5cd40f1e0ab2190
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-04-27 13:48:27 -0500

    correctly flags l33t text

--- Week 4/3398 - Netiquette/Netiquette.java
+++ Week 4/3398 - Netiquette/Netiquette.java
@@ -13,12 +13,31 @@ public class Netiquette {
 			}
 			else {
 				for (int i = 0; i < line.length-1; i++) {
-					if (line[i] == ' ' ){
-						
-					}
-					else if ((int)line[i] >= 65 && (int)line[i] <= 90) {
-						if ((int)line[i+1] >= 65 && (int)line[i+1] <= 90) {
-							status = "suspicious";
+					if (status != "suspicious") {
+						// if ((int)line[i] == 32) {									//Space
+							// if ((int)line[i+1] == ) {									//Single Letter
+							// }
+						// }
+						if ((int)line[i] >= 65 && (int)line[i] <= 90) {				//Uppercase
+							if ((int)line[i+1] >= 65 && (int)line[i+1] <= 90) {			//Uppercase
+								status = "suspicious";
+							}
+							else if ((int)line[i+1] >= 48 && (int)line[i+1] <= 57) {	//Digit
+								status = "suspicious";
+							}
+						}
+						if ((int)line[i] >= 48 && (int)line[i] <= 57) {				//Digit
+							if ((int)line[i+1] >= 65 && (int)line[i+1] <= 90) {			//Uppercase
+								status = "suspicious";
+							}
+							else if ((int)line[i+1] >= 97 && (int)line[i+1] <= 122) {	//Lowercase
+								status = "suspicious";
+							}
+						}
+						if ((int)line[i] >= 97 && (int)line[i] <= 122) {			//Lowercase
+							if ((int)line[i+1] >= 48 && (int)line[i+1] <= 57) {			//Digit
+								status = "suspicious";
+							}
 						}
 					}
 				}
