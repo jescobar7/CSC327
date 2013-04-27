6a2cb17631a025800c27abd42a567c083ee2a166
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-04-27 13:21:46 -0500

    correctly flags shouting text as suspicious

--- Week 4/3398 - Netiquette/Netiquette.java
+++ Week 4/3398 - Netiquette/Netiquette.java
@@ -4,9 +4,27 @@ import java.util.Scanner;
 public class Netiquette {
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
+		String status = "OK";
 		
-		while (sc.hasNextLine()) {
-			System.out.println(sc.nextLine());
+		while (true) {
+			char[] line = sc.nextLine().toCharArray();
+			if (line[0] == '#') {
+				break;
+			}
+			else {
+				for (int i = 0; i < line.length-1; i++) {
+					if (line[i] == ' ' ){
+						
+					}
+					else if ((int)line[i] >= 65 && (int)line[i] <= 90) {
+						if ((int)line[i+1] >= 65 && (int)line[i+1] <= 90) {
+							status = "suspicious";
+						}
+					}
+				}
+				System.out.println(status);
+				status = "OK";
+			}
 		}
 	}
 }
