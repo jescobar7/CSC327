6282680be41e8c268c9695e5180b7c8b1bfcd976
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-04-27 17:39:34 -0500

    added the rest of the cases

--- Week 4/3398 - Netiquette/Netiquette.java
+++ Week 4/3398 - Netiquette/Netiquette.java
@@ -12,31 +12,89 @@ public class Netiquette {
 				break;
 			}
 			else {
-				for (int i = 0; i < line.length-1; i++) {
+				for (int i = 0; i < line.length; i++) {
 					if (status != "suspicious") {
-						// if ((int)line[i] == 32) {									//Space
-							// if ((int)line[i+1] == ) {									//Single Letter
-							// }
-						// }
-						if ((int)line[i] >= 65 && (int)line[i] <= 90) {				//Uppercase
-							if ((int)line[i+1] >= 65 && (int)line[i+1] <= 90) {			//Uppercase
+						int thisChar = (int)line[i];
+						int nextChar = -1;
+						try {nextChar = (int)line[i+1];}catch(Exception e){}
+						int prevChar = -1;
+						try {prevChar = (int)line[i-1];}catch(Exception e){}
+						int nextNext = -1;
+						try {nextNext = (int)line[i+2];}catch(Exception e){}
+						//System.out.println("\nPrevChar:"+prevChar);
+						//System.out.println("ThisChar:"+thisChar);
+						//System.out.println("nextChar:"+nextChar);
+						//System.out.println("NextNext:"+nextNext);
+						
+						if (prevChar==-1 && nextChar==-1) {			//Single Character
+							if (thisChar!=65 && 						//Single Letter no 'a', 'A', 'I'
+								thisChar!=73 && 
+								thisChar!=97) {
 								status = "suspicious";
-							}
-							else if ((int)line[i+1] >= 48 && (int)line[i+1] <= 57) {	//Digit
-								status = "suspicious";
+								break;
 							}
 						}
-						if ((int)line[i] >= 48 && (int)line[i] <= 57) {				//Digit
-							if ((int)line[i+1] >= 65 && (int)line[i+1] <= 90) {			//Uppercase
+						else if (prevChar==32 && nextChar==-1) {	//Last character
+							if (thisChar!=65 && 						//Single Letter no 'a', 'A', 'I'
+								thisChar!=73 && 
+								thisChar!=97) {
 								status = "suspicious";
-							}
-							else if ((int)line[i+1] >= 97 && (int)line[i+1] <= 122) {	//Lowercase
-								status = "suspicious";
+								break;
 							}
 						}
-						if ((int)line[i] >= 97 && (int)line[i] <= 122) {			//Lowercase
-							if ((int)line[i+1] >= 48 && (int)line[i+1] <= 57) {			//Digit
+						else if (prevChar==-1 && nextChar==32) {	//First character
+							if (thisChar!=65 && 						//Single Letter no 'a', 'A', 'I'
+								thisChar!=73 && 
+								thisChar!=97) {
 								status = "suspicious";
+								break;
+							}
+						}
+						else if (thisChar == 32) {					//Space
+							if (nextChar!=65 && 						//Single Letter no 'a', 'A', 'I'
+								nextChar!=73 && 
+								nextChar!=97) {
+								if (nextNext == 32) {
+									status = "suspicious";
+									break;
+								}
+							}
+						}
+						else if ((thisChar==33) ||
+								 (thisChar>=35 && thisChar<=47) || 
+								 (thisChar>=58 && thisChar<=64) ||
+								 (thisChar>=91 && thisChar<=96) ||
+								 (thisChar>=123 && thisChar<=126)) {	//Punctuation
+							if ((nextChar==33) ||
+								 (nextChar>=35 && nextChar<=47) || 
+								 (nextChar>=58 && nextChar<=64) ||
+								 (nextChar>=91 && nextChar<=96) ||
+								 (nextChar>=123 && nextChar<=126)) {
+								 status = "suspicious";
+							}
+						}
+						else if (thisChar>=65 && thisChar<=90) {	//Uppercase
+							if (nextChar>=65 && nextChar<=90) {			//Uppercase
+								status = "suspicious";
+							}
+							else if (nextChar>=48 && nextChar<=57) {	//Digit
+								status = "suspicious";
+								break;
+							}
+						}
+						else if (thisChar>=48 && thisChar<=57) {	//Digit
+							if (nextChar>=65 && nextChar<=90) {			//Uppercase
+								status = "suspicious";
+							}
+							else if (nextChar>=97 && nextChar<=122) {	//Lowercase
+								status = "suspicious";
+								break;
+							}
+						}
+						else if (thisChar>=97 && thisChar<=122) {	//Lowercase
+							if (nextChar>=48 && nextChar<=57) {			//Digit
+								status = "suspicious";
+								break;
 							}
 						}
 					}
