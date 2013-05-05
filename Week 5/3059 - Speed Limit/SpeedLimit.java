a314ce245e039c02f72da2467f88d1796b149cfa
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-05-05 16:31:24 -0500

    added some file reading logic

--- Week 5/3059 - Speed Limit/SpeedLimit.java
+++ Week 5/3059 - Speed Limit/SpeedLimit.java
@@ -5,8 +5,28 @@ public class SpeedLimit {
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		
-		while (sc.hasNextLine()) {
-			System.out.println(sc.nextLine());
+		while (true) {
+			String[] line = sc.nextLine().split(" ");
+			int miles = -1;
+			int rows = -1;
+			int mphOld = -1;
+			int timeOld = -1;
+			int mphNew = -1;
+			int timeNew = -1;
+			
+			if (line.length == 1) {
+				rows = Integer.parseInt(line[0]);
+				if (rows == -1) {
+					break;
+				}
+			}
+			else if (line.length == 2) {
+				if (mphOld == 0) {
+				}
+			}
+			
+			if (miles != -1)
+				System.out.println(miles+" miles");
 		}
 	}
 }
