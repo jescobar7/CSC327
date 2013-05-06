d8e35a77963f5213b060116f6e781d0cf79f8877
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-05-05 19:14:41 -0500

    [3055 - Symmetric Order] Working Solution Submission

--- Week 5/3055 - Symmetric Order/Symmetric.java
+++ Week 5/3055 - Symmetric Order/Symmetric.java
@@ -1,12 +1,53 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Symmetric {
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
+		int set = 0;
+		int numberOfNames = 0;
+		int index = 0;
+		ArrayList<String> names = new ArrayList<String>();
 		
-		while (sc.hasNextLine()) {
-			System.out.println(sc.nextLine());
+		while (true) {
+			String line = sc.nextLine();
+			try {
+				numberOfNames = Integer.parseInt(line);
+				if (numberOfNames == 0) {
+					System.out.println("SET "+set);
+					for (int i = 0; i < names.size(); i++) {
+						System.out.println(names.get(i));
+					}
+					break;
+				}
+				else {
+					if (set != 0) {
+						System.out.println("SET "+set);
+						for (int i = 0; i < names.size(); i++) {
+							System.out.println(names.get(i));
+						}
+					}
+					set++;
+					index = 0;
+					names = new ArrayList<String>();
+				}
+			} catch (NumberFormatException e) {
+				index = names.size();
+				if (index == 0) {
+					names.add(index, line);
+				}
+				else if (index == 1) {
+					names.add(index, line);
+				}
+				else {
+					double tmpIndex = names.size();
+					tmpIndex = tmpIndex/2;
+					tmpIndex = Math.ceil(tmpIndex);
+					index = (int)tmpIndex;
+					names.add(index,line);
+				}
+			}
 		}
 	}
 }
