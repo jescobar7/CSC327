07a9169bc875e08e125893563abcc1a0ef33792c
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-05-19 21:29:42 -0500

    [2789 - Tournament] Initialized App

---
+++ Week 7/2789 - Tournament/Tournament.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Tournament {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+	}
+}
