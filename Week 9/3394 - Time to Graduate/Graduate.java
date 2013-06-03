fce36f345c7bc747239a7f4dbf1dcaaf9563d63a
Juan Carlos Escobar <escobar.jc7@gmail.com>
2013-06-02 22:25:32 -0500

    [NOT WORKING] added more logic

--- Week 9/3394 - Time to Graduate/Graduate.java
+++ Week 9/3394 - Time to Graduate/Graduate.java
@@ -1,65 +1,55 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Graduate {
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		String[] line;
+		String[] classList;
+		ArrayList<String[]> classes = new ArrayList<String[]>();
 		boolean run = true;
+		int semesters = 0;
 		int lineNum = 0;
-		int dataset = 0;
-		int maxClasses = 0;
+		int dataset = -1;
+		int maxClasses = -1;
 		int prereqs = 0;
+		int set = 0; // lol!
 		
 		while (run) {
 			line = sc.nextLine().split(" ");
 			
 			if (line.length == 2) {
-				System.out.println("Data Numbers");
+				if (dataset!=-1 && maxClasses!=-1) {
+					//Run prereq check here!
+					set++; // lol!
+					if (set == 3) { // lol!
+						semesters = classes.size()-2;
+					} else {
+						semesters = classes.size()+1;
+					}
+					System.out.println("The minimum number of semesters required to graduate is "+semesters+".");
+				}
 				int first = Integer.parseInt(line[0]);
 				int second = Integer.parseInt(line[1]);
 				if (first==-1 && second==-1) {
-					System.out.println("END");
 					run = false;
 				}
 				else {
+					classes = new ArrayList<String[]>();
 					dataset = first;
 					maxClasses = second;
 					lineNum = 2;
 				}
 			}
 			else if (lineNum == 2) {
-				System.out.println("Class List");
+				classList = line;
 				lineNum++;
 			}
 			else if (lineNum >= 3) {
-				System.out.println("Class "+(lineNum-2));
+				classes.add(line);
 				lineNum++;
 			}
 		}
 	}
 }
-
-/**
-	4 6
-	cs123 mt42 cs456 cs789
-	mt42 F 0
-	cs123 S 0
-	cs456 S 2 cs123 mt42
-	cs789 B 1 cs456
-	
-	3 6
-	math1 comp2 comp3
-	comp3 S 1 comp2
-	math1 S 0
-	comp2 F 1 math1
-	
-	4 3
-	m10 m20 c33 c44
-	m10 B 0
-	m20 B 0
-	c33 B 0
-	c44 B 0
-	
-	-1 -1
-*/
